431cbb9aaebfd682045b8a1919af01e1abe29615
Cody James <33857557+codyjamestechnical@users.noreply.github.com>
2022-07-13 00:57:16 -0400

    further debug

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,13 @@
 // config/plugins.js
 
-module.exports = ({env}) => ({
-  //...
-  console.log(env("MEILI_MASTER_KEY", "masterkey"));
-    meilisearch: {
-        config: {
-        // Your meili host
-        host: "localhost:7700",
-        // Your master key or private key
-        apiKey: env("MEILI_MASTER_KEY", "masterKey"),
-        },
-    },
-});
+// module.exports = ({env}) => ({
+//   //..
+//     meilisearch: {
+//         config: {
+//         // Your meili host
+//         host: "localhost:7700",
+//         // Your master key or private key
+//         apiKey: env("MEILI_MASTER_KEY", "masterKey"),
+//         },
+//     },
+// });
