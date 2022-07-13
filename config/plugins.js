73aa7238434160867d105bcef78e507abc57d028
Cody James <33857557+codyjamestechnical@users.noreply.github.com>
2022-07-13 01:56:57 -0400

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({env}) => ({
     meilisearch: {
         config: {
         // Your meili host
-        host: "http://localhost/",
+        host: "http://localhost:7700/",
         // Your master key or private key
         apiKey: env("MEILI_MASTER_KEY", "masterKey"),
         },
