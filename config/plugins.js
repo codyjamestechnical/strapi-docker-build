9a49ef8b92ce26f17a06513a3f24654ff342fd68
Cody James <33857557+codyjamestechnical@users.noreply.github.com>
2022-07-13 00:48:19 -0400

    fixed env pull

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,13 @@
 // config/plugins.js
 
-module.exports = () => ({
+module.exports = ({env}) => ({
   //...
     meilisearch: {
         config: {
         // Your meili host
         host: "http://127.0.0.1:7700",
         // Your master key or private key
-        apiKey: process.env.MEILI_MASTER_KEY || "masterKey",
+        apiKey: env("MEILI_MASTER_KEY", "masterKey"),
         },
     },
 });
